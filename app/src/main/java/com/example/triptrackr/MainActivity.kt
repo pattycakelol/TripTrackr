c4117f250483f4e9d8f8c09e0dacc847f2f14c58
pattycakelol <pleung1415@gmail.com>
2019-09-14 23:43:00 -0700

    working on connecting activities + kotlin

--- app/src/main/java/com/example/triptrackr/MainActivity.kt
+++ app/src/main/java/com/example/triptrackr/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.triptrackr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    val friends_button : Button = findViewById(R.id.view_friends) as Button
+
+    friends_button.setOnClickListener {
+        val intent = Intent(this, Friends::class.java)
+        startActivity(intent)
     }
 }
