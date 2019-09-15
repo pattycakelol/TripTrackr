f3365ce7c886c1681a197e1c9fd6172ff0065475
pattycakelol <pleung1415@gmail.com>
2019-09-15 00:06:36 -0700

    jk kotlin is sick

--- app/src/main/java/com/example/triptrackr/MainActivity.kt
+++ app/src/main/java/com/example/triptrackr/MainActivity.kt
@@ -1,33 +1,23 @@
 package com.example.triptrackr
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val friendsButton : Button = findViewById(R.id.view_friends) as Button
+
+        friendsButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity, Friends::class.java))
+        }
     }
-
-
-
-    val friends_button : Button = findViewById(R.id.view_friends);
-
-    friends_button.
-
-//    friends_button.setOnClickListener (object: View.OnClickListener {
-//        override fun onClick(view: View): Unit {
-//            startActivity(Intent(this, Friends::class.java))
-//        }
-//
-//        private fun Intent(onClickListener: View.OnClickListener, java: Class<Friends>): Context {
-//
-//        }
-//    })
 }
+
+
